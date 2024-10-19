c5fed1b849dbb43efe245ce566f29b9cc8b60274
Leonardo Meza <168940126+LeoM-Dev@users.noreply.github.com>
2024-10-19 09:17:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,56 @@
 const form = document.getElementById('satisfactionForm');
 const ratings = [
-    { id: 'rating1', feedbackId: 'feedback1' },
-    { id: 'rating2', feedbackId: 'feedback2' },
-    { id: 'rating3', feedbackId: 'feedback3' }
+    { id: 'rating1', feedbackId: 'feedback1', reactionId: 'reaction1' },
+    { id: 'rating2', feedbackId: 'feedback2', reactionId: 'reaction2' },
+    { id: 'rating3', feedbackId: 'feedback3', reactionId: 'reaction3' }
 ];
 
 ratings.forEach(rating => {
     const slider = document.getElementById(rating.id);
     const feedback = document.getElementById(rating.feedbackId);
+    const reaction = document.getElementById(rating.reactionId);
 
     slider.addEventListener('input', function() {
         const value = slider.value;
         let feedbackText = '';
+        let reactionEmoji = ''; 
+        let feedbackColor = '#eaf1e6'; 
+        let feedbackBackgroundColor = '#3e73d4';
 
         switch(value) {
-            case '1': feedbackText = 'Malo'; break;
-            case '2': feedbackText = 'Poco Satisfecho'; break;
-            case '3': feedbackText = 'Satisfecho'; break;
-            case '4': feedbackText = 'Muy Satisfecho'; break;
-            case '5': feedbackText = 'Sumamente Satisfecho'; break;
+            case '1': 
+                feedbackText = 'Malo'; 
+                reactionEmoji = '&#128532';
+                feedbackColor = '#FDFFFC';
+                feedbackBackgroundColor = '#D62839';
+                break;
+            case '2': 
+                feedbackText = 'Poco Satisfecho'; 
+                reactionEmoji = '&#128577';
+                feedbackColor = '#FDFFFC'; 
+                feedbackBackgroundColor = '#FFC100';
+                break;
+            case '3': 
+                feedbackText = 'Satisfecho'; 
+                reactionEmoji = '&#128512';
+                break;
+            case '4': 
+                feedbackText = 'Muy Satisfecho'; 
+                reactionEmoji = '&#128513';
+                feedbackColor = '#FDFFFC';
+                feedbackBackgroundColor = '#5FAD41';
+                break;
+            case '5': 
+                feedbackText = 'Sumamente Satisfecho'; 
+                reactionEmoji = '&#128525';
+                feedbackColor = '#FDFFFC';
+                feedbackBackgroundColor = '#98CE00';
+                break;
         }
 
         feedback.textContent = feedbackText;
+        feedback.style.color = feedbackColor;
+        feedback.style.backgroundColor = feedbackBackgroundColor;
+        reaction.innerHTML = reactionEmoji; 
     });
-});
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const review = document.getElementById('review').value;
-
-    const rating1 = document.getElementById('rating1').value;
-    const rating2 = document.getElementById('rating2').value;
-    const rating3 = document.getElementById('rating3').value;
-
-    const mailtoLink = `mailto:hilarygomezrodriguez836@gmail.com?subject=Reseña%20de%20${name}&body=
-    Nombre: ${name}%0D%0A
-    Atención al cliente: ${rating1}%0D%0A
-    Rapidez en el servicio: ${rating2}%0D%0A
-    Satisfacción con el precio: ${rating3}%0D%0A
-    Reseña: ${review || 'No se dejó reseña.'}`;
-
-    window.location.href = mailtoLink;
 });
